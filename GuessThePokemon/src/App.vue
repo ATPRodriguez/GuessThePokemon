72ac85dd71f93d3fa1c19f20392d4505aa7189f8
ATPRodriguez <alejandrotomaspachecorodriguez@gmail.com>
2025-02-19 22:28:14 +0000

    feat: Agregado reto 2

---
+++ GuessThePokemon/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PokemonGame from './views/PokemonGame.vue';
+
+</script>
+
+<template>
+ <PokemonGame/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ GuessThePokemon/src/views/PokemonGame.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl">Espere por favor</h1>
+        <h3 class="animate-pulse">Cargando Pokémons</h3>
+    </section>
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl">¿Cual es este Pokémon?</h1>
+    </section>
+</template>
+
+<script setup lang="ts">
+    import "@/assets/styles.css"
+</script>
+
+<style></style>
